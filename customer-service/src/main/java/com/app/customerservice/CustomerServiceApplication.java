a000504edc1d2c85af43d3eb6c61d8e3fa1a98ba
jnroot0101 <r_jnah@etu.enset-media.ac.ma>
2025-11-01 14:09:33 +0100

    Add project files

---
+++ customer-service/src/main/java/com/app/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,42 @@
+package com.app.customerservice;
+
+import com.app.customerservice.models.Customer;
+import com.app.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(CustomerRepository customerRepository) {
+        return args -> {
+            Customer c1 = Customer.builder()
+                    .firstName("John")
+                    .lastName("Smith")
+                    .email("john.smith@example.com")
+                    .build();
+            Customer c2 = Customer.builder()
+                    .firstName("Adam")
+                    .lastName("Marven")
+                    .email("adam.marven@example.com")
+                    .build();
+
+            List<Customer> customers = new ArrayList<>();
+            customers.add(c1);
+            customers.add(c2);
+
+            customerRepository.saveAll(customers);
+        };
+    }
+
+}
---
+++ customer-service/src/main/java/com/app/customerservice/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.app.customerservice.repositories;
+
+import com.app.customerservice.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
